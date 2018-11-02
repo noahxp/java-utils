fb55519e8546ef502783194b9e7c4b7030fb7a54
Noah Liu <noahxp@gmail.com>
2018-11-02 20:54:42 +0800

    fix spring active profiles with system environment

--- src/main/java/tw/noah/utils/yaml/YamlUtils.java
+++ src/main/java/tw/noah/utils/yaml/YamlUtils.java
@@ -2,6 +2,7 @@ package tw.noah.utils.yaml;
 
 import java.io.InputStream;
 import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -39,6 +40,7 @@ public class YamlUtils {
 
   /**
    * read yaml profile , support spring profiles.
+   *
    * @param is yaml file input-stream object
    * @return all yaml properties
    */
@@ -55,16 +57,27 @@ public class YamlUtils {
 
     List<String> profileNames = new ArrayList<>();
 
-    if (propsList.size() > 1) { // multi profile check the spring.profiles.active
-      propsList.forEach((p) -> {
-        if (p.containsKey("spring.profiles.active")) {  // 第一組
-          if (p.get("spring.profiles.active").getClass() == ArrayList.class) { // 多個 profile
-            profileNames.addAll((ArrayList) p.get("spring.profiles.active"));
-          } else {
-            profileNames.add(p.get("spring.profiles.active").toString());
+    if (propsList.size() > 1) { // multi profile, get the whot is active profiles
+      String profiles = null;
+      if (System.getProperty("spring.profiles.active") != null && System.getProperty("spring.profiles.active").trim().length() > 0) {  // -Dspring.profiles.active
+        profiles = System.getProperty("spring.profiles.active");
+      } else if (System.getenv().get("spring.profiles.active") != null && System.getenv().get("spring.profiles.active").trim().length() > 0) {  // os environment
+        profiles = System.getenv().get("spring.profiles.active");
+      }
+      if (profiles != null) {
+        String[] profilesArray = profiles.split(",");
+        profileNames.addAll(Arrays.asList(profilesArray));
+      } else {  // no special setting on os or jvm , use the applicaton.yml settings.
+        propsList.forEach((p) -> {
+          if (p.containsKey("spring.profiles.active")) {  // first profiles.
+            if (p.get("spring.profiles.active").getClass() == ArrayList.class) { // multi profiles
+              profileNames.addAll((ArrayList) p.get("spring.profiles.active"));
+            } else { // single profiles
+              profileNames.add(p.get("spring.profiles.active").toString()); // the active profiles.
+            }
           }
-        }
-      });
+        });
+      }
     }
 
     propsList.forEach((p) -> {
